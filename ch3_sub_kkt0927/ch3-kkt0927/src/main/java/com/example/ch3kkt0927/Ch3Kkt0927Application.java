16cac09ebeb52fc0b5f20ec2668c3db207340071
김경태 <kkt8274@gmail.com>
2023-09-27 11:18:12 +0900

    0927 김경태 실습과제

---
+++ ch3_sub_kkt0927/ch3-kkt0927/src/main/java/com/example/ch3kkt0927/Ch3Kkt0927Application.java
@@ -0,0 +1,13 @@
+package com.example.ch3kkt0927;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3Kkt0927Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch3Kkt0927Application.class, args);
+	}
+
+}
---
+++ ch3_sub_kkt0927/ch3-kkt0927/src/test/java/com/example/ch3kkt0927/Ch3Kkt0927ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch3kkt0927;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3Kkt0927ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
